ef09d37f6004144fc2c47898469e5ef8fafd1eb9
willrburge <156019241+willrburge@users.noreply.github.com>
2024-09-10 15:39:38 -0400

    Add project files.

---
+++ MIST353F24FrontEnd/Pages/Taco.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MIST353F24FrontEnd.Pages
+{
+    public class TacoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
